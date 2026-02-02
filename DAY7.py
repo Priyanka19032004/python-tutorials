5b0b3442916df1dc84a39bd2439b11acdd6dcb92
Priyanka <19.priyankadumal@gmail.com>
2026-02-02 17:14:15 +0530

    completed

---
+++ DAY7.py
@@ -0,0 +1,73 @@
+fruit_stock = {
+    "Apple":{"price":20, "quantity":30},
+    "Orange":{"price":30, "quantity":40},
+    "Grapes":{"price":40, "quantity":50}
+}
+
+def show_fruit():
+    for fruit in fruit_stock:
+        print("fruit:",fruit)
+        print("price:",fruit_stock[fruit]["price"])
+        print("quantity:",fruit_stock[fruit]["quantity"])
+        print("--------------------------------")
+    
+def add_fruit():
+    name = input("enter fruit name:")
+    if name == "":
+        print("fruit name is not empty:")
+        return
+    try:
+        price = int(input("enter the price of fruit:"))
+        quantity = int(input("enter the quantity of fruit:"))
+    except:
+        print("price and quantity should be number:")
+        return
+    fruit_stock[name]={"price": price, "quantity":quantity}
+    print("fruit added:")
+    print("--------------------")
+    
+def update_quantity():
+    name = input("enter fruits name:")
+    if name in fruit_stock:
+        try:
+            new_quantity = int(input("enter new quantity:"))
+        except:
+            print("quantity should be number:")
+            return
+        
+        fruit_stock[name]["quantity"] = new_quantity
+        print("quantity updated:")
+    else:
+        print("quantity failed")
+    print("----------------------")
+def delete_fruit():
+    name = input("Enter fruit name to delete: ")
+
+    if name in fruit_stock:
+        del fruit_stock[name]
+        print("✅ Fruit deleted successfully!")
+    else:
+        print("❌ Fruit not found!")
+    
+while True:
+    print("1. show_fruit:")
+    print("2. add_fruit:")
+    print("3. update_fruit:")
+    print("4. delete_fruit:")
+    print("5.exit:")
+     
+    choice = input("enter your choice:")
+     
+    if choice == "1":
+        show_fruit()
+    elif choice == "2":
+        add_fruit()
+    elif choice == "3":
+        update_quantity()
+    elif choice == "4":
+        delete_fruit()
+    elif choice == "5":
+        print("exit:")
+        break
+    else:
+        print("not updated")
